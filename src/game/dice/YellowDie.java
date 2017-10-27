dd748f9910511b96eef413710ee609269a03cf57
CaptainBanana <32339173+CaptainBanana@users.noreply.github.com>
2017-10-27 14:02:29 -0600

    Update YellowDie.java

--- src/game/dice/YellowDie.java
+++ src/game/dice/YellowDie.java
@@ -4,4 +4,17 @@ package game.dice;
  * @author Kenneth Stebbins - kstebbins@student.neumont.edu
  */
 public class YellowDie extends Die{
+  public Roll roll()
+    {
+        int roll = getRoll();
+        if(roll <= 2) {
+            return Roll.BRAIN;
+        }else if(roll <= 4){
+            return Roll.RUNNER;
+        }else {
+            return Roll.SHOT;
+        }
+    }
+
+    public String name() {return  "Yellow Die";}
 }
