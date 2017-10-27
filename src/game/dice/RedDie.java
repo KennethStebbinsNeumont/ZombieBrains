bf586d57f251111a6882f2590053f1300108a773
CaptainBanana <32339173+CaptainBanana@users.noreply.github.com>
2017-10-27 14:01:07 -0600

    Update RedDie.java

--- src/game/dice/RedDie.java
+++ src/game/dice/RedDie.java
@@ -4,4 +4,17 @@ package game.dice;
  * @author Kenneth Stebbins - kstebbins@student.neumont.edu
  */
 public class RedDie extends Die {
+   public Roll roll()
+    {
+        int roll = getRoll();
+        if(roll <= 1) {
+            return Roll.BRAIN;
+        }else if(roll <= 3){
+            return Roll.RUNNER;
+        }else {
+            return Roll.SHOT;
+        }
+    }
+
+    public String name() {return  "Red Die";}
 }
